cdb4d143a0cc842270bb196b66dab54cc856b7e9
NowWeAre-Free <kontodarm@wp.pl>
2023-07-01 18:51:05 +0200

    add reset li elements button

--- main.js
+++ main.js
@@ -4,14 +4,18 @@ let orderElement = 1;
 const init = () => {
   // tutaj kod
   const btn = document.createElement("button");
+  const btnReset = document.createElement("button");
   const ul = document.createElement("ul");
 
   btn.textContent = "Dodaj 10 elementów listy";
+  btnReset.textContent = "Wyczyść";
 
   document.body.appendChild(btn);
+  document.body.appendChild(btnReset);
   document.body.appendChild(ul);
 
   btn.addEventListener("click", createLiElements);
+  btnReset.addEventListener("click", removeElements);
 };
 
 const createLiElements = () => {
@@ -23,6 +27,12 @@ const createLiElements = () => {
     li.style.fontSize = `${size++}px`;
     ul.appendChild(li);
   }
+};
+
+const removeElements = () => {
+  document.querySelector("ul").textContent = "";
+  size = 10;
+  orderElement = 1;
 };
 
 init();
